e6f69a1c989a1b049ce20549b4c0969b145e7e59
mrask2 <mrask2@illinois.edu>
2024-12-04 22:28:17 -0600

    made reach

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/C2cDMZND8sBtrSxUti6G4"
+        width="100%"
+        style={{ height: '100%', minHeight: '700px'}}
+        frameBorder="0"
+        title="CPS Chatbot"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
